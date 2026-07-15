3fcdfda658a0e740c6b2b7524f86858bfaf5a85b
RaymondLee <112743925+Raym0nd1ee@users.noreply.github.com>
2026-07-15 11:33:36 +0800

    Update app.js

--- static/app.js
+++ static/app.js
@@ -183,7 +183,6 @@ get isAllFilteredSelected() {
     if (filteredIndices.length === 0) return false;
     return filteredIndices.every(idx => this.selectedCsvIndices.includes(idx));
 },
-        ...createQcMethods,
         selectDimensionFiles(e) {
 
             const files =
